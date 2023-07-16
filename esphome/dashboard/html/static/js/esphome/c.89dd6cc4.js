2849ba943e876b576934a27b4c0b3e760147d00b
Graham Brown <grahambrown11@gmail.com>
2023-07-16 20:22:32 +0200

    trying to fix routing for ingress

--- esphome/dashboard/html/static/js/esphome/c.89dd6cc4.js
+++
@@ -1,8 +0,0 @@
-import{b as e,j as s,s as l,x as o}from"./index-fbcc8d20.js";import"./c.ee97463d.js";import"./c.3e732c4c.js";let a=class extends l{render(){return o`
-      <esphome-process-dialog
-        heading="Update All"
-        .type=${"update-all"}
-        @closed=${this._handleClose}
-      ></esphome-process-dialog>
-    `}_handleClose(){this.parentNode.removeChild(this)}};a=e([s("esphome-update-all-dialog")],a);
-//# sourceMappingURL=c.89dd6cc4.js.map

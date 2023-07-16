121fcb92782d7132b2cfa1ba7323bae95d87cf9a
Graham Brown <grahambrown11@gmail.com>
2023-07-16 21:19:56 +0200

    trying to fix routing for ingress

--- esphome/dashboard/html/static/js/esphome/c.7ecff4fd.js
+++
@@ -1,2 +0,0 @@
-import{m as r,A as s}from"./index-656b1697.js";const t=()=>r(s.resolvePath("/serial-ports")).then((r=>r.filter((r=>"OTA"!==r.port))));export{t as g};
-//# sourceMappingURL=c.7ecff4fd.js.map
---
+++ esphome/dashboard/html/static/js/esphome/c.c4eae155.js
@@ -0,0 +1,2 @@
+import{m as r,A as s}from"./index-7a45a5bf.js";const t=()=>r(s.resolvePath("/serial-ports")).then((r=>r.filter((r=>"OTA"!==r.port))));export{t as g};
+//# sourceMappingURL=c.c4eae155.js.map
